7a752a229808ffe016ff66aa553eb7ee4e886a7e
Julius Fält Skoglund <julius.faltskoglund@gmail.com>
2026-09-23 10:06:21 +0200

    test

--- extract/weather.py
+++ extract/weather.py
@@ -1,5 +1,5 @@
 import requests
-import pandas as pd 
+import pandas as pd
 
 CLIMATE_URL = "https://climate-api.open-meteo.com/v1/climate"
 FLOOD_URL = "https://flood-api.open-meteo.com/v1/flood"
@@ -11,6 +11,7 @@ cities = {
     "Uppsala": (59.8586, 17.6389),
     "Kiruna": (67.8558, 20.2253),
 }
+
 
 def get_climate_data(city, latitude, longitude):
     params = {
@@ -27,15 +28,46 @@ def get_climate_data(city, latitude, longitude):
         ),
         "timezone": "Europe/Stockholm",
     }
-    response = requests.get(CLIMATE_URL, params=params)
-    response.raise_for.status()
+    print("About to requets...")
+    response = requests.get(CLIMATE_URL, params=params, timeout=30)
+    print("Got response:", response.status_code)
+    response.raise_for_status()  
 
     data = response.json()
-
     df = pd.DataFrame(data["daily"])
-
     df["city"] = city
     df["latitude"] = latitude
     df["longitude"] = longitude
-
     return df
+
+
+def get_flood_data(city, latitude, longitude):
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "start_date": "2020-01-01",
+        "end_date": "2024-12-31",
+        "daily": "river_discharge",
+        "timezone": "Europe/Stockholm",
+    }
+    print("About to request...")
+    response = requests.get(FLOOD_URL, params=params, timeout=30)
+    print("Got response", response.status_code)
+    response.raise_for_status()
+
+    data = response.json()
+    df = pd.DataFrame(data["daily"])
+    df["city"] = city
+    df["latitude"] = latitude
+    df["longitude"] = longitude
+    return df
+
+
+if __name__ == "__main__":
+    city, (lat, lon) = "Stockholm", cities["Stockholm"]
+
+    print("Climate:")
+    print(get_climate_data(city, lat, lon).head())
+
+    print("\nFlood:")
+    print(get_flood_data(city, lat, lon).head())
